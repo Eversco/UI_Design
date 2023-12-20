cb545fc08955814935911382eff3b08a69e71532
Nathan0922 <nathan920922@gmail.com>
2023-12-20 17:27:05 +0800

    加了可以選擇hider或seeker的按鈕在按下start之後

--- UI_Design/Assets/Scripts/Menu/BttonClickScript.cs
+++ UI_Design/Assets/Scripts/Menu/BttonClickScript.cs
@@ -5,7 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class ButtonClickScript : MonoBehaviour
 {
-    public void StartGame()
+
+    public void StartGameAsSeeker()
+    {
+
+        SceneManager.LoadScene(1);
+
+    }
+    public void StartGameAsHider()
     {
         SceneManager.LoadScene(1);
     }
@@ -15,13 +22,4 @@ public class ButtonClickScript : MonoBehaviour
         Application.Quit();
     }
 
-    public void Setting()
-    {
-
-    }
-
-    public void Help()
-    {
-
-    }
 }
